653b403b6c8afa52f25a55130b3848c9f2a81ac8
IsaelMachuca <izaelmachuca22@gmail.com>
2023-10-31 10:35:10 -0700

    1

---
+++ ProyectoCrud.DAL/Repositorio/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositorio
+{
+    public interface IGenericRepository<T>where T: class
+    {
+        Task<bool> Insertar (T modelo);
+        Task<bool> Actualizar(T modelo);
+        Task<T> Obtener(int Id);
+        Task<bool> Eliminar(int Id);
+        Task<IQueryable<T>> ObtenerTodo();
+    }
+}
--- ProyectoCrud.Models/DataContext/DbcrudcoreContext.cs
+++ ProyectoCrud.Models/DataContext/DbcrudcoreContext.cs
@@ -17,7 +17,6 @@ public partial class DbcrudcoreContext : DbContext
 
     public virtual DbSet<Contacto> Contactos { get; set; }
 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.; DataBase=DBCRUDCORE;Trusted_Connection=true;TrustServerCertificate=true");
