110a6d66b170638bc672c1afc5ec484ec394dadb
cisco01 <cisco01@CISCO1>
2023-10-31 09:18:04 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoCrud.Models/DataContext/DbcrudcoreContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoCrud.Models.DataContext;
+
+public partial class DbcrudcoreContext : DbContext
+{
+    public DbcrudcoreContext()
+    {
+    }
+
+    public DbcrudcoreContext(DbContextOptions<DbcrudcoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.; DataBase=DBCRUDCORE;Trusted_Connection=true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.IdContacto).HasName("PK__Contacto__A4D6BBFA4AA5C8EB");
+
+            entity.ToTable("Contacto");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
